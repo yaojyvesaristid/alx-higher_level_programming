d07c56716917e1594f4f97f379775d74a1ec17fb
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-10-03 10:24:58 -0700

    edit solution for 10)

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@ def divisible_by_2(my_list=[]):
     if not my_list:
         return my_list
     nlist = list(my_list)
-    for i in nlist:
-        if i % 2 == 0:
+    for i in range(len(nlist)):
+        if nlist[i] % 2 == 0:
             nlist[i] = True
         else:
             nlist[i] = False
