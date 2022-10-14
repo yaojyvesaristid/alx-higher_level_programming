035a87a70231c00de924631e7d64ec8bf96faee6
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-10-14 09:41:13 -0700

    edit solution for 2- by introducing set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if len(my_list) > 0:
-        nlist = list()
-        for val in my_list:
-            if val not in nlist:
-                nlist.append(val)
-                sum = 0
-        while len(nlist) > 0:
-            sum = sum + nlist.pop()
+        my_set = set(my_list)
+        sum = 0
+        while len(my_set) > 0:
+            sum = sum + my_set.pop()
         return sum
     return 0
