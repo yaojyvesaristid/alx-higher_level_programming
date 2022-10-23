73314ea54adc89c2acaa94ea481f5755077a1241
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-23 15:27:24 -0700

    add solution for 2-

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count, no_int = 0, 0
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+
+            print()
+            return count
+
+        except (TypeError, ValueError):
+            pass
+
+        except IndexError:
+            return count
