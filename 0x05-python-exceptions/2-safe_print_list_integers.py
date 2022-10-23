68188838052a774c84117cdacb7fb10603ef50d9
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-23 15:33:27 -0700

    edit solution for 2-

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
             count += 1
-
-            print()
-            return count
+        print()
+        return count
 
     except (TypeError, ValueError):
         pass
