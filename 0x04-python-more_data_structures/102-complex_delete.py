161e84d9df6494d7feaa6fdb661e8c69161f455d
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-23 11:40:15 -0700

    add solution for 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+def complex_delete(a_dictionary, value):
+    if value not in a_dictionary.values():
+        return a_dictionary
+    val_to_delete = []
+    for k, v in a_dictionary.items():
+        if v == value:
+            val_to_delete.append(k)
+    for val in val_to_delete:
+        del a_dictionary[val]
+    return a_dictionary
