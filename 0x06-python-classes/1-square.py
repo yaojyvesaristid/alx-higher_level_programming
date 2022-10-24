da55e64a7ff50f10822f06ee4b31312c531821f6
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-24 16:08:32 -0700

    add solution for 1-

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is a square module which define a square class
+"""
+
+
+class Square:
+    """ square class with his size and his constructor"""
+    def __init__(self, size):
+        sel.__size = size
