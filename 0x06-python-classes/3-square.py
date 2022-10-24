e85e63bb12ba024fc3165cd29adf68e9a1f63870
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-24 16:44:15 -0700

    add solution for 3-

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is a square module which define a square class
+"""
+
+
+class Square:
+    """ square class with private instance attribute size
+        size must be a digit and have a value >= 0
+        method area return the square value of attribute size
+    """
+    def __init__(self, size=0):
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size**2
