7e984d4c56d8ee18af37bb0f73b7a1b94c82c52f
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-24 16:11:05 -0700

    edit solution with correct syntax

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,4 +7,4 @@ This is a square module which define a square class
 class Square:
     """ square class with his size and his constructor"""
     def __init__(self, size):
-        sel.__size = size
+        self.__size = size
