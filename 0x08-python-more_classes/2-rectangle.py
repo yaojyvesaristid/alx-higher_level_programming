edade69ba06a29830f9a747175d6043beac2c847
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-26 17:36:23 -0700

    add solution for 2-

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class with attribute width and height.
+Default values of both attributes are 0.
+"""
+
+
+class Rectangle:
+    """
+    A Rectangle class with attributes  width and height
+    constructor and getters and setters for the 2 attributes
+    method area to compute the rectangle area
+    metho perimeter to compute the rectangle perimeter
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__width == 0:
+            return 0
+        return (self.__width + self.__height) * 2
