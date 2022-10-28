a1615ff233c3c47a869cc47537f85cb269e4b591
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-28 07:56:52 -0700

    edit solution with doc

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""
-A rectangle with width and height.
+"""This is the "Rectangle"  module.
+This module provides a Rectangle class.
 """
 
 
 class Rectangle:
-    """
-    Rectangle functions and data
+    """A Rectangle class with attributes width and height,
+    methods area, perimeter, print, str, repr, and del, and
+    class attribute number_of_instances that keeps track of # of instances,
+    class attribute print_symbol which is used as symbol for printing,
+    static method bigger_or_equal that returns biggest rectangle,
+    and class method square that returns a new Rectangle.
     """
 
     number_of_instances = 0
