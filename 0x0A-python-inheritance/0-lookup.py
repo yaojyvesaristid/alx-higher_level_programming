0916c9f192c183d24877f4fa5c3ccc2dcfd2d578
yaojyvesarsitid <yaojyvesaristide@gmail.com>
2022-10-31 16:03:15 -0700

    add soution for 0-

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Lookup
+"""
+
+
+def lookup(obj):
+    """Function that returns the list of available
+    attributes and methods of an object.
+    """
+
+    return dir(obj)
