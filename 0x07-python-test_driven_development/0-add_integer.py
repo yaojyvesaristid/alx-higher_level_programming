002405b2f5e88ab60611d49367a9835ce845349b
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-11-02 12:31:07 +0000

    edit solution for 0-

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,18 +4,18 @@ Module Add integer
 """
 
 
-def add_integer(a, b=89):
-	"""
-	method add integer with two params
-	a and b must be integers of floats, otherwise raise a TypeError
-	a and b must be first casted to integer if they are float
-	return the addition of a and b as an integer
-	"""
+def add_integer(a, b=98):
+    """
+    method add integer with two params
+    a and b must be integers of floats, otherwise raise a TypeError
+    a and b must be first casted to integer if they are float
+    return the addition of a and b as an integer
+    """
 
-	if type(a) != int or type(a) != float:
-		raise TypeError("a msut be an integer")
-	
-	if type(b) != int or type(b) != float:
-		raise TypeError("b must be an integer")
+    if type(a) != int or type(a) != float:
+        raise TypeError("a msut be an integer")
 
-	return int(a) + int(b)
+    if type(b) != int or type(b) != float:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
