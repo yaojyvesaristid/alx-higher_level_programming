6b6afe63a7dafa7b8681a7ef8d7bab751f2d3399
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-11-03 12:49:58 +0000

    add solution for 101-

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module Add Attribute
+"""
+
+
+def add_attribute(obj, name, value):
+    """
+    add new attribute to an object 'obj' if it's possible
+    """
+
+    immuable_object = [int, float, str, dict, list, tuple, object]
+    if type(obj) in immuable_object:
+        raise TypeError("can't add new attribute")
+
+    obj.__setattr__(name, value)
