288d015c4d1edbcc34f73bf194bcdd669fe485a1
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-11-03 12:18:31 +0000

    edit solution for 10 by modifying init method of 9-

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,8 +17,8 @@ class Rectangle(BaseGeometry):
     """
 
     def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
