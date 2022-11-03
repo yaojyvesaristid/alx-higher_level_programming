e7283e09825cacbc3b480d83e5df06bbd1936a6c
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-11-03 12:01:03 +0000

    add solution for 10-

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module Square
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square inherits from Rectangle
+    size must be private and be validate by integer_validator
+    """
+
+    def __init__(self, size):
+        super().integer_value("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
