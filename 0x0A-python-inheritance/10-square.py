ff4cada7febb9f083d4d607d360989e44ee87fbf
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-11-03 12:04:51 +0000

    edit solution for 10-

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,6 +13,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         super().integer_value("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
