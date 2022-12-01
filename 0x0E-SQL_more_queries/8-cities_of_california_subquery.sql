a188b61b5dbfe0cb26d236c93e04838d185e9f16
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-11-30 17:23:18 -0800

    edit solution for 8-

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,6 @@
 -- results must be sorted in ascending order by cities.id
 -- not allowed to user JOIN
 USE hbtn_0d_usa;
-SELECT * FROM cities c
-WHERE c.state_id = (SELECT states.id FROM states s WHERE s.name = 'California')
+SELECT cities.id, cities.name FROM cities c, states s
+WHERE c.state_id = s.id AND s.name = 'California'
 ORDER BY c.id ASC;
