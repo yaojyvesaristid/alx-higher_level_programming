ade8ecd607cdbd2fb1b8023a4907200e53d447c8
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-11-30 16:14:45 -0800

    edit solution for 1-

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates the mysql user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_11'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
