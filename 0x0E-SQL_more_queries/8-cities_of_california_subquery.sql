c2a433263bfe68331b709348216f19b8e993f5e7
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-11-30 17:36:26 -0800

    edit solution for 8-

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,6 @@
 -- lists all the cities of California
 -- results must be sorted in ascending order by cities.id
--- not allowed to user JOIN
-USE hbtn_0d_usa;
+-- not allowed to use JOIN
 SELECT cities.id, cities.name
 FROM cities, states
 WHERE cities.state_id = states.id
