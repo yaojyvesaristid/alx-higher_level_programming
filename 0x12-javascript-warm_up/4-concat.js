710933be771af6f6e41e8350fc393e63bd4e31b8
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-12-19 23:58:24 +0000

    add solution for 4-

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,9 +4,9 @@
 let var1 = 'undefined';
 let var2 = 'undefined';
 if (process.argv.length === 3) {
-    var1 = process.argv[2];
+  var1 = process.argv[2];
 } else if (process.argv.length > 3) {
-    var1 = process.argv[2];
-    var2 = process.argv[3];
+  var1 = process.argv[2];
+  var2 = process.argv[3];
 }
 console.log(`${var1} is ${var2}`);
