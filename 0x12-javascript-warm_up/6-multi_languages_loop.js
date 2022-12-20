5faa42b45107317581d85cd045b1835408729d7e
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-12-20 01:39:14 +0000

    change let by const

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let tab = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const tab = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < tab.length; i++) {
   console.log(tab[i]);
 }
