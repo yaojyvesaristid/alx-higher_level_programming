0760feee337a0c0871444208b959cbe42f05ea15
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-12-21 13:34:04 +0000

    edit solution for 100-

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 let liste = require('./100-data').list;
 console.log(liste);
-liste = liste.map((i, j) => {
-  return i * j;
-});
+liste = liste.map((i, j) => i * j);
 console.log(liste);
