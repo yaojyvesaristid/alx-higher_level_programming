48829a2eb9cfbef78d171dc9b2d1a0e4f853cfd8
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-12-21 02:17:31 +0000

    add solution for 8-

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let temp;
+  const len = list.length;
+  for (let i = 0; i < len / 2; i++) {
+    temp = list[i];
+    list[i] = list[len - (1 + i)];
+    list[len - (1 + i)] = temp;
+  }
+  return list;
+}
